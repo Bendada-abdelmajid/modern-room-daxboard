c740663ab753bcee2f5e619ce415336077ae6729
Bendada-abdelmajid <82829092+Bendada-abdelmajid@users.noreply.github.com>
2023-05-08 12:41:44 +0100

    add licence key

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import { registerLicense } from '@syncfusion/ej2-base';
 
+// Registering Syncfusion license key
+registerLicense('ORg4AjUWIQA/Gnt2VFhhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5Wdk1jXntfcHJWT2ZZ');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
